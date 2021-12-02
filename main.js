ab1679e1e01c98a9763ba85c6bfb2df64373dcac
KrishKonda <77596750+KrishKonda@users.noreply.github.com>
2021-12-01 20:46:54 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song="";
 song2="";
+leftWristY=0;
+leftWristX=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
     song=loadSound("music.mp3");
     song2=loadSound("music2.mp3");
@@ -9,7 +13,23 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
 }
 function draw(){
 image(video,0,0,600,500);
+}
+function modelLoaded(){
+    console.log("Posenet is initialized");
+}
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    rightWristX=results[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    console.log("leftWristX="+leftWristX+",leftWristY="+leftWristY);
+console.log("rightWristX="+rightWristX+",rightWristY="+rightWristY);
+}
 }
